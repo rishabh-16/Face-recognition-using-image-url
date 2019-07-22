caacae388315d7b6621d4e5747cde34ddc5d501a
rishabh-16 <akchitra99@gmail.com>
2019-07-22 22:11:19 +0530

    initial commit

---
+++ utils.py
@@ -0,0 +1,247 @@
+import tensorflow as tf
+import numpy as np
+import os
+from numpy import genfromtxt
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+import glob
+from PIL import Image
+import requests
+from io import BytesIO
+import matplotlib.pyplot as plt
+import cv2
+
+_FLOATX = 'float32'
+
+def variable(value, dtype=_FLOATX, name=None):
+    v = tf.Variable(np.asarray(value, dtype=dtype), name=name)
+    _get_session().run(v.initializer)
+    return v
+
+def shape(x):
+    return x.get_shape()
+
+def square(x):
+    return tf.square(x)
+
+def zeros(shape, dtype=_FLOATX, name=None):
+    return variable(np.zeros(shape), dtype, name)
+
+def concatenate(tensors, axis=-1):
+    if axis < 0:
+        axis = axis % len(tensors[0].get_shape())
+    return tf.concat(axis, tensors)
+
+def LRN2D(x):
+    return tf.nn.lrn(x, alpha=1e-4, beta=0.75)
+
+def conv2d_bn(x,layer=None,cv1_out=None,cv1_filter=(1, 1),
+              cv1_strides=(1, 1),cv2_out=None,cv2_filter=(3, 3),
+              cv2_strides=(1, 1),padding=None):
+    num = '' if cv2_out == None else '1'
+    tensor = Conv2D(cv1_out, cv1_filter, strides=cv1_strides, name=layer+'_conv'+num)(x)
+    tensor = BatchNormalization(axis=3, epsilon=0.00001, name=layer+'_bn'+num)(tensor)
+    tensor = Activation('relu')(tensor)
+    if padding == None:
+        return tensor
+    tensor = ZeroPadding2D(padding=padding)(tensor)
+    if cv2_out == None:
+        return tensor
+    tensor = Conv2D(cv2_out, cv2_filter, strides=cv2_strides, name=layer+'_conv'+'2')(tensor)
+    tensor = BatchNormalization(axis=3, epsilon=0.00001, name=layer+'_bn'+'2')(tensor)
+    tensor = Activation('relu')(tensor)
+    return tensor
+
+weights = [
+  'conv1', 'bn1', 'conv2', 'bn2', 'conv3', 'bn3',
+  'inception_3a_1x1_conv', 'inception_3a_1x1_bn',
+  'inception_3a_pool_conv', 'inception_3a_pool_bn',
+  'inception_3a_5x5_conv1', 'inception_3a_5x5_conv2', 'inception_3a_5x5_bn1', 'inception_3a_5x5_bn2',
+  'inception_3a_3x3_conv1', 'inception_3a_3x3_conv2', 'inception_3a_3x3_bn1', 'inception_3a_3x3_bn2',
+  'inception_3b_3x3_conv1', 'inception_3b_3x3_conv2', 'inception_3b_3x3_bn1', 'inception_3b_3x3_bn2',
+  'inception_3b_5x5_conv1', 'inception_3b_5x5_conv2', 'inception_3b_5x5_bn1', 'inception_3b_5x5_bn2',
+  'inception_3b_pool_conv', 'inception_3b_pool_bn',
+  'inception_3b_1x1_conv', 'inception_3b_1x1_bn',
+  'inception_3c_3x3_conv1', 'inception_3c_3x3_conv2', 'inception_3c_3x3_bn1', 'inception_3c_3x3_bn2',
+  'inception_3c_5x5_conv1', 'inception_3c_5x5_conv2', 'inception_3c_5x5_bn1', 'inception_3c_5x5_bn2',
+  'inception_4a_3x3_conv1', 'inception_4a_3x3_conv2', 'inception_4a_3x3_bn1', 'inception_4a_3x3_bn2',
+  'inception_4a_5x5_conv1', 'inception_4a_5x5_conv2', 'inception_4a_5x5_bn1', 'inception_4a_5x5_bn2',
+  'inception_4a_pool_conv', 'inception_4a_pool_bn',
+  'inception_4a_1x1_conv', 'inception_4a_1x1_bn',
+  'inception_4e_3x3_conv1', 'inception_4e_3x3_conv2', 'inception_4e_3x3_bn1', 'inception_4e_3x3_bn2',
+  'inception_4e_5x5_conv1', 'inception_4e_5x5_conv2', 'inception_4e_5x5_bn1', 'inception_4e_5x5_bn2',
+  'inception_5a_3x3_conv1', 'inception_5a_3x3_conv2', 'inception_5a_3x3_bn1', 'inception_5a_3x3_bn2',
+  'inception_5a_pool_conv', 'inception_5a_pool_bn',
+  'inception_5a_1x1_conv', 'inception_5a_1x1_bn',
+  'inception_5b_3x3_conv1', 'inception_5b_3x3_conv2', 'inception_5b_3x3_bn1', 'inception_5b_3x3_bn2',
+  'inception_5b_pool_conv', 'inception_5b_pool_bn',
+  'inception_5b_1x1_conv', 'inception_5b_1x1_bn',
+  'dense_layer'
+]
+
+conv_shape = {
+  'conv1': [64, 3, 7, 7],
+  'conv2': [64, 64, 1, 1],
+  'conv3': [192, 64, 3, 3],
+  'inception_3a_1x1_conv': [64, 192, 1, 1],
+  'inception_3a_pool_conv': [32, 192, 1, 1],
+  'inception_3a_5x5_conv1': [16, 192, 1, 1],
+  'inception_3a_5x5_conv2': [32, 16, 5, 5],
+  'inception_3a_3x3_conv1': [96, 192, 1, 1],
+  'inception_3a_3x3_conv2': [128, 96, 3, 3],
+  'inception_3b_3x3_conv1': [96, 256, 1, 1],
+  'inception_3b_3x3_conv2': [128, 96, 3, 3],
+  'inception_3b_5x5_conv1': [32, 256, 1, 1],
+  'inception_3b_5x5_conv2': [64, 32, 5, 5],
+  'inception_3b_pool_conv': [64, 256, 1, 1],
+  'inception_3b_1x1_conv': [64, 256, 1, 1],
+  'inception_3c_3x3_conv1': [128, 320, 1, 1],
+  'inception_3c_3x3_conv2': [256, 128, 3, 3],
+  'inception_3c_5x5_conv1': [32, 320, 1, 1],
+  'inception_3c_5x5_conv2': [64, 32, 5, 5],
+  'inception_4a_3x3_conv1': [96, 640, 1, 1],
+  'inception_4a_3x3_conv2': [192, 96, 3, 3],
+  'inception_4a_5x5_conv1': [32, 640, 1, 1,],
+  'inception_4a_5x5_conv2': [64, 32, 5, 5],
+  'inception_4a_pool_conv': [128, 640, 1, 1],
+  'inception_4a_1x1_conv': [256, 640, 1, 1],
+  'inception_4e_3x3_conv1': [160, 640, 1, 1],
+  'inception_4e_3x3_conv2': [256, 160, 3, 3],
+  'inception_4e_5x5_conv1': [64, 640, 1, 1],
+  'inception_4e_5x5_conv2': [128, 64, 5, 5],
+  'inception_5a_3x3_conv1': [96, 1024, 1, 1],
+  'inception_5a_3x3_conv2': [384, 96, 3, 3],
+  'inception_5a_pool_conv': [96, 1024, 1, 1],
+  'inception_5a_1x1_conv': [256, 1024, 1, 1],
+  'inception_5b_3x3_conv1': [96, 736, 1, 1],
+  'inception_5b_3x3_conv2': [384, 96, 3, 3],
+  'inception_5b_pool_conv': [96, 736, 1, 1],
+  'inception_5b_1x1_conv': [256, 736, 1, 1],
+}
+
+def load_weights():
+    # Set weights path
+    dirPath = './weights'
+    fileNames = filter(lambda f: not f.startswith('.'), os.listdir(dirPath))
+    paths = {}
+    weights_dict = {}
+
+    for n in fileNames:
+        paths[n.replace('.csv', '')] = dirPath + '/' + n
+
+    for name in weights:
+        if 'conv' in name:
+            conv_w = genfromtxt(paths[name + '_w'], delimiter=',', dtype=None)
+            conv_w = np.reshape(conv_w, conv_shape[name])
+            conv_w = np.transpose(conv_w, (2, 3, 1, 0))
+            conv_b = genfromtxt(paths[name + '_b'], delimiter=',', dtype=None)
+            weights_dict[name] = [conv_w, conv_b]     
+        elif 'bn' in name:
+            bn_w = genfromtxt(paths[name + '_w'], delimiter=',', dtype=None)
+            bn_b = genfromtxt(paths[name + '_b'], delimiter=',', dtype=None)
+            bn_m = genfromtxt(paths[name + '_m'], delimiter=',', dtype=None)
+            bn_v = genfromtxt(paths[name + '_v'], delimiter=',', dtype=None)
+            weights_dict[name] = [bn_w, bn_b, bn_m, bn_v]
+        elif 'dense' in name:
+            dense_w = genfromtxt(dirPath+'/dense_w.csv', delimiter=',', dtype=None)
+            dense_w = np.reshape(dense_w, (128, 736))
+            dense_w = np.transpose(dense_w, (1, 0))
+            dense_b = genfromtxt(dirPath+'/dense_b.csv', delimiter=',', dtype=None)
+            weights_dict[name] = [dense_w, dense_b]
+
+    return weights_dict
+
+def image_to_embedding(image, model):
+    #image = cv2.resize(image, (96, 96), interpolation=cv2.INTER_AREA) 
+    image = cv2.resize(image, (96, 96)) 
+    img = image[...,::-1]
+    img = np.around(np.transpose(img, (0,1,2))/255.0, decimals=12)
+    x_train = np.array([img])
+    embedding = model.predict_on_batch(x_train)
+    return embedding
+
+def recognize_face(face_image, input_embeddings, model, threshold = 100, verbose = 1):
+
+    embedding = image_to_embedding(face_image, model)
+    
+    minimum_distance = 200
+    name = None
+    
+    # Loop over  names and encodings.
+    for (input_name, input_embedding) in input_embeddings.items():
+        euclidean_distance = np.linalg.norm(embedding-input_embedding)
+        if(verbose == 1):
+            print('Euclidean distance from %s is %s' %(input_name, euclidean_distance))
+
+        if euclidean_distance < minimum_distance:
+            minimum_distance = euclidean_distance
+            name = input_name
+    
+    if minimum_distance < threshold:
+        return str(name)
+    else:
+        return None
+    
+def create_input_image_embeddings(model):
+    input_embeddings = {}
+
+    for file in glob.glob("images/*"):
+        person_name = os.path.splitext(os.path.basename(file))[0]
+        image_file = cv2.imread(file, 1)
+        input_embeddings[person_name] = image_to_embedding(image_file, model)
+
+    return input_embeddings
+
+def recognize_faces_in_image(input_embeddings, model, verbose):
+    url = input('Enter URL of the image\n')
+    response = requests.get(str(url))
+    img = np.array(Image.open(BytesIO(response.content)))
+    img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+    height, width, channels = img.shape
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    identities = []
+    for (x, y, w, h) in faces:
+        x1 = x
+        y1 = y
+        x2 = x+w
+        y2 = y+h
+        face_image = img[max(0, y1):min(height, y2), max(0, x1):min(width, x2)]
+        identity = recognize_face(face_image, input_embeddings, model,verbose = verbose)
+        if identity is not None:
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            img = cv2.rectangle(img,(x1, y1),(x2, y2),(255,255,255),2)
+            cv2.putText(img, str(identity), (x1+5,y1-5), font, 1, (255,255,255), 2)
+    plt.imshow(img[...,::-1])
+
+def store_faces():
+    url = input('Enter url of the image\n')
+    count = 0
+    response = requests.get(str(url))
+    img = np.array(Image.open(BytesIO(response.content)))
+    img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
+    height, width, channels = img.shape
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    fig = plt.figure(figsize=(8, 8))
+    rows = 1
+    columns = len(faces)
+    images = []
+    for (x,y,w,h) in faces:
+        x1 = x
+        y1 = y
+        x2 = x+w
+        y2 = y+h     
+        count += 1
+        img1 = img[y1:y2,x1:x2]
+        images.append(img1)
+        fig.add_subplot(rows, columns, count)
+        plt.imshow(img1[...,::-1])
+    plt.show()
+    for i in images:
+        name = input()
+        cv2.imwrite("images/"+str(name)+".jpg", i)
